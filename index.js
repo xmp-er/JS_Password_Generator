ed04bb2ed5adce0375cdd0b4042fba262bde179e
xmp-er <bankofhawkgatsu@gmail.com>
2023-02-28 12:22:13 +0100

    basic elements caught

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 pass_box=document.querySelector("[pass_box]")//manipulate value using .value
+pass_len=document.querySelector("[pass_len]")
+slider=document.querySelector("[slider]")
+uppercase=document.querySelector("[uppercase]")
+lowercase=document.querySelector("[lowercase]")
+numbers=document.querySelector("[numbers]")
+symbols=document.querySelector("[symbols]")
+//add event listener to the generate_password button
+
