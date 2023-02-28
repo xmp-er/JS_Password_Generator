96902b5e9c818f1f0d2f546c0a171f2925a47775
xmp-er <bankofhawkgatsu@gmail.com>
2023-02-28 12:42:35 +0100

    Generated the random functions

--- index.js
+++ index.js
@@ -7,3 +7,41 @@ numbers=document.querySelector("[numbers]")
 symbols=document.querySelector("[symbols]")
 //add event listener to the generate_password button
 
+//function to change display of password-length everytime slide is interacted
+function slider_connect(){
+    pass_len.textContent=slider.value
+}
+//adding event listener to slider
+slider.addEventListener('change',slider_connect);
+
+//function to generate random uppercase letter and return it
+function get_rand_up(){
+    let a = Math.floor(Math.random()*(26))
+    return String.fromCharCode(65+a)
+}
+
+
+//function to generate random lowercase letter and return it
+function get_rand_lower(){
+    let a = Math.floor(Math.random()*(26))
+    return String.fromCharCode(97+a)
+}
+
+//function to generate random number
+function get_rand_num(){
+    let a = Math.floor(Math.random()*(10))
+    return String.fromCharCode(48+a)
+}
+
+let special_chars="@(~!@#$%^&*_-+=`|\(){}[]:;'<>,.?/)"
+//function to generate special characters
+function get_rand_char(){
+    let a = Math.floor(Math.random()*(special_chars.length))
+    return (special_chars[a])
+}
+
+
+//function to generate password
+function generate_pass(){
+    func_arr=[uppercase]
+}
